aaf994836f2cb94c4b45928a2b08bd452a7ba2cf
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2011-05-29 22:20:28 +0200

    reader/writer protobuf

--- src/main/java/nfrancois/poc/protobuf/resource/ProtobufMessageBodyReader.java
+++ src/main/java/nfrancois/poc/protobuf/resource/ProtobufMessageBodyReader.java
@@ -13,7 +13,6 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.Provider;
 
-
 import com.google.protobuf.GeneratedMessage;
 import com.google.protobuf.Message;
 
@@ -21,21 +20,18 @@ import com.google.protobuf.Message;
 @Consumes("application/x-protobuf")
 public class ProtobufMessageBodyReader implements MessageBodyReader<Message> {
 
-	  public boolean isReadable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
-		  return Message.class.isAssignableFrom(type);
-	  }
+	public boolean isReadable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+		return Message.class.isAssignableFrom(type);
+	}
 
- 
-      public Message readFrom(Class<Message> type, Type genericType, Annotation[] annotations,
-              MediaType mediaType, MultivaluedMap<String, String> httpHeaders, 
-              InputStream entityStream) throws IOException, WebApplicationException {
-      try {
-          Method newBuilder = type.getMethod("newBuilder");
-          GeneratedMessage.Builder builder = (GeneratedMessage.Builder) newBuilder.invoke(type);
-          return builder.mergeFrom(entityStream).build();
-      } catch (Exception e) {
-          throw new WebApplicationException(e);
-      }
-  }
+	public Message readFrom(Class<Message> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, String> httpHeaders, InputStream entityStream) throws IOException, WebApplicationException {
+		try {
+			Method newBuilder = type.getMethod("newBuilder");
+			GeneratedMessage.Builder builder = (GeneratedMessage.Builder) newBuilder.invoke(type);
+			return builder.mergeFrom(entityStream).build();
+		} catch (Exception e) {
+			throw new WebApplicationException(e);
+		}
+	}
 
 }
---
+++ src/main/java/nfrancois/poc/protobuf/resource/ProtobufMessageBodyWriter.java
@@ -0,0 +1,44 @@
+package nfrancois.poc.protobuf.resource;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+
+import com.google.protobuf.Message;
+
+@Provider
+@Produces("application/x-protobuf")
+public class ProtobufMessageBodyWriter implements MessageBodyWriter<Message> {
+	public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+		return Message.class.isAssignableFrom(type);
+	}
+
+	private Map<Object, byte[]> buffer = new WeakHashMap<Object, byte[]>();
+
+	public long getSize(Message m, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try {
+			m.writeTo(baos);
+		} catch (IOException e) {
+			return -1;
+		}
+		byte[] bytes = baos.toByteArray();
+		buffer.put(m, bytes);
+		return bytes.length;
+	}
+
+	public void writeTo(Message m, Class type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+		entityStream.write(buffer.remove(m));
+	}
+}
